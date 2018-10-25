70991e7066d635821db82edf02c3ad649b58dd7d
abill <abill@DESKTOP-QCC9QS1.home>
2018-10-25 16:53:50 -0300

    principal

---
+++ organico4you-api/src/main/java/br/com/organico4you/api/Organico4youApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.organico4you.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Organico4youApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Organico4youApiApplication.class, args);
+	}
+}
