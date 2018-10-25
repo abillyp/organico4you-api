33317845690447f87fb0ba57cb5ec3e25e8dc964
abill <abill@DESKTOP-QCC9QS1.home>
2018-10-25 16:55:11 -0300

    1

--- organico4you-api/src/main/java/br/com/organico4you/api/Organico4youApiApplication.java
+++ organico4you-api/src/main/java/br/com/organico4you/api/Organico4youApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Organico4youApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Organico4youApiApplication.class, args);
+		SpringApplication.run(Organico4youApiApplication.class, args); 
 	}
 }
