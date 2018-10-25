85fa8eaf1231dab8b74c3476e3fc6bd153076e5a
abill <abill@DESKTOP-QCC9QS1.home>
2018-10-25 20:19:28 -0300

    2

---
+++ organico4you-api/src/main/java/br/com/organico4you/api/model/Item.java
@@ -0,0 +1,72 @@
+package br.com.organico4you.api.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="item")
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	
+	private String descricao;
+	
+	private BigDecimal valor;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	
+}
---
+++ organico4you-api/src/main/java/br/com/organico4you/api/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package br.com.organico4you.api.repository;
+
+import org.springframework.data.repository.Repository;
+
+import br.com.organico4you.api.model.Item;
+
+public class ItemRepository implements Repository<Item, Long> {
+
+}
